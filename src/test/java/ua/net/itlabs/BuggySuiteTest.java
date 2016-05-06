eae0b51dbdcb50699aeaa19270ce21a5f9ae82ab
ljubanka <lvpopl@gmail.com>
2016-05-06 19:44:25 +0300

    initial commit

---
+++ src/test/java/ua/net/itlabs/BuggySuiteTest.java
@@ -0,0 +1,14 @@
+package ua.net.itlabs;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import ua.net.itlabs.categories.Buggy;
+import ua.net.itlabs.operations.TodosOperationsAtAllFilter;
+
+@RunWith(Categories.class)
+@Suite.SuiteClasses(TodosOperationsAtAllFilter.class)
+@Categories.IncludeCategory(Buggy.class)
+public class BuggySuiteTest {
+}
